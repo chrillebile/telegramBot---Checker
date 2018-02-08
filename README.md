f9988c6319f7d3de342d291f5eb2af18f1c2cb36
chrillebile <chrillebile@gmail.com>
2018-02-08 16:00:44 +0100

    Moved bots and added README
    - Moved all bots to a bots folder.
    - Added README.md
    - Uppdated .gitignore

---
+++ README.md
@@ -0,0 +1,8 @@
+# Telegram Bot
+
+Playing around with different APIs, connecting them to my Telegram bot so you can do searches and more in Telegram. Written in Python.
+
+API used:
+- [Telegram](https://core.telegram.org/bots/api)
+- [Wolfram|Alpha](https://products.wolframalpha.com/api/)
+- Google Search
--- main.py
+++ main.py
@@ -1,28 +1,45 @@
-import checkerBot, wolframBot, googleBot
+from bots import checkerBot, wolframBot, googleBot
 from time import sleep
+
+last_update_id = None
+
 
 def inlineCommand(updates):
     for update in updates["result"]:
         try:
-            text, chat_id = checkerBot.get_last_chat_id_and_text(updates)
-            if text.startswith("/start"):
-                text = "Hey " + update["message"]["from"]["first_name"]
-            elif text.startswith("/ask"):
-                text = wolframBot.ask(text)
-            elif text.startswith("/google"):
-                text = googleBot.googleSearch(text)
-            checkerBot.send_message(text, chat_id)
+            if(update["inline_query"]["query"] != None):
+                text = update["inline_query"]["query"]
+                chat_id = update["inline_query"]["from"]["id"]
+                sendMessage("Sry cannot search for: " + text + ". This method isn't working yet. Use /ask instead.", chat_id)
+            else:
+                text, chat_id = checkerBot.get_last_chat_id_and_text(updates)
+                if text.startswith("/start"):
+                    sendMessage("Hey " + update["message"]["from"]["first_name"], chat_id)
+                elif text.startswith("/ask"):
+                    sendMessage(wolframBot.ask(text), chat_id)
+                elif text.startswith("/google"):
+                    sendMessage(googleBot.googleSearch(text), chat_id)
         except Exception as e:
-            print(e)
+            print(update)
+
+
+def sendMessage(text, chat_id):
+    checkerBot.send_message(text, chat_id)
+
+
+def getMessage():
+    global last_update_id
+    updates = checkerBot.get_updates(last_update_id)
+    if len(updates["result"]) > 0:
+        last_update_id = checkerBot.get_last_update_id(updates) + 1
+        inlineCommand(updates)
+
 
 def main():
-    last_update_id = None
     while True:
-        updates = checkerBot.get_updates(last_update_id)
-        if len(updates["result"]) > 0:
-            last_update_id = checkerBot.get_last_update_id(updates)+1
-            inlineCommand(updates)
+        getMessage()
         sleep(0.5)
+
 
 if __name__ == '__main__':
     main()
